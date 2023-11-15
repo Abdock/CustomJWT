1a3d4836d05f267f8fe0217eb9ea52e7be8ee5f2
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-11-15 17:53:19 +0600

    Added simple implementation for JWT

---
+++ JwtAuth.Jwt/JwtTokenProvider.cs
@@ -0,0 +1,35 @@
+﻿using JwtAuth.Abstractions;
+using JwtAuth.Abstractions.TokenComponents;
+using JwtAuth.Jwt.Extensions;
+
+namespace JwtAuth.Jwt;
+
+public class JwtTokenProvider : ITokenProvider
+{
+    private readonly ISecuredHashingAlgorithm _hashingAlgorithm;
+
+    public JwtTokenProvider(ISecuredHashingAlgorithm hashingAlgorithm)
+    {
+        _hashingAlgorithm = hashingAlgorithm;
+    }
+
+    public string GenerateJwt(TokenInfo info)
+    {
+        var header = new Header
+        {
+            Algorithm = _hashingAlgorithm.AlgorithmName,
+            TokenType = "JWT"
+        };
+        var payload = new Payload
+        {
+            Audience = info.Audience,
+            Issuer = info.Issuer,
+            Expiration = info.ExpirationDate.UtcDateTime.Ticks,
+            CustomClaims = info.CustomClaims.ToList()
+        };
+        var signature = _hashingAlgorithm.ComputeHash(info.SecuredKey);
+        var encodedHeader = header.ToBase64String();
+        var encodedPayload = payload.ToBase64String();
+        return $"{encodedHeader}.{encodedPayload}.{signature}";
+    }
+}
---
+++ JwtAuth.Jwt/SecuredHashingAlgorithms/HmacSha256Algorithm.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using JwtAuth.Abstractions;
+
+namespace JwtAuth.Jwt.SecuredHashingAlgorithms;
+
+public class HmacSha256Algorithm : ISecuredHashingAlgorithm
+{
+    private readonly Encoding _encoding;
+    private readonly HashAlgorithm _algorithm;
+
+    public HmacSha256Algorithm()
+    {
+        _encoding = Encoding.UTF8;
+        _algorithm = new HMACSHA256();
+    }
+
+    public HmacSha256Algorithm(Encoding encoding) : this()
+    {
+        _encoding = encoding;
+    }
+
+    public HmacSha256Algorithm(Encoding encoding, byte[] key) : this(encoding)
+    {
+        _algorithm = new HMACSHA256(key);
+    }
+
+    public string AlgorithmName => HashingAlgorithms.HmacSha256;
+
+    public string ComputeHash(string data)
+    {
+        var encryptedKey = _encoding.GetBytes(data);
+        var hashedData = _algorithm.ComputeHash(encryptedKey);
+        return Convert.ToBase64String(hashedData);
+    }
+}
