6cab4dc0a4e4637d002d812c5e53b3b361727e64
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2023-11-15 17:53:43 +0600

    Add example

---
+++ JwtAuth.Example.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using JwtAuth.Abstractions;
+using JwtAuth.Jwt;
+using JwtAuth.Jwt.SecuredHashingAlgorithms;
+
+ISecuredHashingAlgorithm hashingAlgorithm = new HmacSha256Algorithm();
+ITokenProvider tokenProvider = new JwtTokenProvider(hashingAlgorithm);
+var tokenInfo = new TokenInfo
+{
+    Audience = "localAudience",
+    Issuer = "localIssuer",
+    ExpirationDate = DateTimeOffset.Now.AddHours(1),
+    SecuredKey = "MySuperSecuredKey"
+};
+var jwt = tokenProvider.GenerateJwt(tokenInfo);
+Console.WriteLine(jwt);
